d8819cdbeaf5035f04348ff643ceae98e9758e6d
texiwustion <w6666s@outlook.com>
2023-10-30 07:39:37 +0800

    feat(大厅): 帖子卡片

--- src/pages/lobby/index.tsx
+++ src/pages/lobby/index.tsx
@@ -17,19 +17,46 @@ function Box() {
       title: "卡片三",
       text1: "一楼：这是一楼的内容",
       text2: "二楼：这是二楼的内容"
+    },
+    {
+      title: "卡片三",
+      text1: "一楼：这是一楼的内容",
+      text2: "二楼：这是二楼的内容"
+    },
+    {
+      title: "卡片三",
+      text1: "一楼：这是一楼的内容",
+      text2: "二楼：这是二楼的内容"
+    },
+    {
+      title: "卡片三",
+      text1: "一楼：这是一楼的内容",
+      text2: "二楼：这是二楼的内容"
     }
   ];
 
   return (
-    <div className="m-4 p-4 bg-gray-200 rounded">
+    <div className="p2 rounded flex flex-col">
       {cards.map((card, index) => (
-        <div key={index} className="bg-white p-4 rounded shadow mb-4">
-          <h4 className="text-lg font-bold mb-2">{card.title}</h4>
-          <p>{card.text1}</p>
-          <p>{card.text2}</p>
-        </div>
+        // <div key={index} className="bg-white p-4 rounded shadow mb-4">
+        //   <h4 className="text-lg font-bold mb-2">{card.title}</h4>
+        //   <p>{card.text1}</p>
+        //   <p>{card.text2}</p>
+        // </div>
+        <div key={index} className="flex flex-col m3 section h120">
+      <span className="self-start text">开发商跑路了怎么办？</span>
+      <div className="divider"></div>
+      <div className="self-start group_2">
+        <span className="font_1">
+          {card.text1}
+          <br />
+        </span>
+        <span className="font_1">{card.text2}</span>
+      </div>
+    </div>
       ))}
     </div>
+    
   );
 }
 export default function Lobby() {
@@ -39,7 +66,6 @@ export default function Lobby() {
 
   return (
     <View className="lobby">
-      <Text>Hello GenShin!</Text>
       <Box />
     </View>
   );
