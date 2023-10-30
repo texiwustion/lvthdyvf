7c77d2b7718753f2ac883e4dfd334c09211e8f1a
texiwustion <w6666s@outlook.com>
2023-10-30 10:09:22 +0800

    feat(tabBar): 添加tabBar图标

--- src/app.config.ts
+++ src/app.config.ts
@@ -17,7 +17,7 @@ export default defineAppConfig({
   ],
   window: {
     backgroundTextStyle: "light",
-    navigationBarBackgroundColor: "#fff",
+    navigationBarBackgroundColor: "#f1f1f1",
     navigationBarTitleText: "律堂鼎臻",
     navigationBarTextStyle: "black"
   },
@@ -25,22 +25,32 @@ export default defineAppConfig({
     list: [
       {
         pagePath: "pages/index/index",
+        iconPath: "./static/首页.png",
+        selectedIconPath: "./static/首页.png",
         text: "资源页"
       },
       {
         pagePath: "pages/main/index",
+        iconPath: "./static/首页.png",
+        selectedIconPath: "./static/首页2.png",
         text: "首页"
       },
       {
         pagePath: "pages/lobby/index",
+        iconPath: "./static/大厅.png",
+        selectedIconPath: "./static/大厅2.png",
         text: "大厅"
       },
       {
         pagePath: "pages/search/index",
+        iconPath: "./static/检索.png",
+        selectedIconPath: "./static/检索2.png",
         text: "检索"
       },
       {
         pagePath: "pages/profile/index",
+        iconPath: "./static/我的.png",
+        selectedIconPath: "./static/我的2.png",
         text: "我的"
       }
     ],
