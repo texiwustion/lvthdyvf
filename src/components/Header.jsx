57d8bf094d73649a3490ae207048b4dc3f0a6e33
texiwustion <w6666s@outlook.com>
2023-10-30 10:10:26 +0800

    feat(Header): 添加Header模块

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
-export function Header() {
-  return <div className="center text-2xl text-blue">律堂鼎臻</div>;
-}
+const Header = () => (
+  <div className="center text-2xl text-blue bg-hex-f1f1f1">律堂鼎臻</div>
+);
+
+export default Header;
