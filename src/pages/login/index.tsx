b175f9fefe1df21c6889b72b2ec03d379b0cbff3
texiwustion <w6666s@outlook.com>
2023-11-01 20:15:45 +0800

    feat(登录页): 仿制登录跳转与注册成功

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -11,8 +11,46 @@ export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = () => {
-    // 登录逻辑
+  const toMain = (): void => {
+    Taro.switchTab({ url: "/pages/main/index" });
+  };
+
+  const checkData = (): Boolean => {
+    if (username == "" || password == "") return false;
+    return true;
+  };
+  const handleLogin = async () => {
+    if (!checkData()) {
+      return Taro.showToast({
+        title: `表单不能为空！`,
+        icon: "error",
+        duration: 2000
+      });
+    }
+
+    Taro.setStorageSync("username", username);
+    Taro.setStorageSync("password", password);
+
+    if (pageTheme == "登录") {
+      await Taro.showToast({
+        title: `成功${pageTheme}`,
+        icon: "success",
+        duration: 2000
+      });
+      setTimeout(toMain, 1500);
+    } else {
+      await Taro.showModal({
+        title: "提示",
+        content: "注册成功！是否跳转到登录页？",
+        success: function(res) {
+          if (res.confirm) {
+            setTimeout(() => setPageTheme("登录"), 400);
+          } else if (res.cancel) {
+            console.log("用户点击取消");
+          }
+        }
+      });
+    }
   };
 
   return (
@@ -25,14 +63,18 @@ export default function Login() {
             name="username"
             placeholder="用户名或手机号"
             className="input-item"
+            onChange={e => setUsername(e.currentTarget.value)}
           />
           <input
             type="password"
             name="password"
             placeholder="密码"
             className="input-item"
+            onChange={e => setPassword(e.currentTarget.value)}
           />
-          <div className="btn bg-main-l">{pageTheme}</div>
+          <div className="btn bg-main-l" onClick={handleLogin}>
+            {pageTheme}
+          </div>
         </div>
         <div className="msg no-wrap center">
           {pageTheme == "登录" ? "还没有账号？" : "已有账号？"}
